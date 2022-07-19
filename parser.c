1b2327eddde7443e499d3590b371dda2c9ffa7b1
riomonet <arizablozki@gmail.com>
2022-07-19 19:47:19 -0400

    completed legacy parser

--- parser.c
+++ parser.c
@@ -15,7 +15,7 @@ struct in {
 
 struct out {
 	
-	char			amount[17];
+	char			*amount;
 	char			*pubkey;
 
 };
@@ -71,8 +71,6 @@ int main(int argc, char *argv[])
 	int  inp_cnt = strtoul(tx.inputs, &p, 16);
 	tx.in_arr = malloc(inp_cnt * sizeof(struct in));
 
-
-        printf("\n" );
 	/* inputs array routine  */
         for (i = 0; i < inp_cnt ; ++i) {
 
@@ -105,18 +103,59 @@ int main(int argc, char *argv[])
 		tx.outputs[i] = *ptr++;
 	}
 	tx.outputs[2] = '\0';
+        int output_cnt = strtoul(tx.outputs, &p, 16);
 
-	printf("\n%s\n%s\n",tx.version,tx.inputs);
+	tx.out_arr = malloc(output_cnt * sizeof(struct out));
+
+        for (i = 0; i < output_cnt; ++i) {
+
+		tx.out_arr[i].amount = malloc(16);
+                for (j = 0; j < 16; ++j) 
+			tx.out_arr[i].amount[j] = *ptr++;
+
+                for (j = 0; j < 2; ++j) {
+			tmp[j] = *ptr++;
+		}
+		tmp[2] = '\0';
+
+		script_len = strtoul(tmp, &p, 16);
+                tx.out_arr[i].pubkey = malloc((2 * script_len + 1));
+
+                for (j = 0; j < script_len * 2; ++j) 
+			tx.out_arr[i].pubkey[j] = *ptr++;
+                
+	}
+
+	for (i = 0; i < 8; ++i) {
+		tx.locktime[i] = *ptr++;
+	}
+	tx.locktime[i] = '\0';
+        
+
+
+        printf("\n\tverison:\t%s\n\n\tinput_cnt:\t%s",tx.version,tx.inputs);
 
         for (i = 0; i < inp_cnt ; ++i) {
 		printf("\n__________________________________\n");
-		printf("%d\n",i);
+		printf("%d\n",i + 1);
 		printf("txid:\t%s\n", tx.in_arr[i].txid);
 		printf("vout:\t%s\n", tx.in_arr[i].vout);
 	
 		printf("sc_sig:\t%s\n", tx.in_arr[i].script_sig);
 		printf("seq:\t%s\n", tx.in_arr[i].sequence);
-	}
+        }
+
+        printf("\n\toutput_cnt:\t%s",tx.outputs);
+
+        for (i = 0; i < output_cnt ; ++i) {
+		printf("\n__________________________________\n");
+		printf("%d\n",i + 1);
+                printf("amount:\t%s\n", tx.out_arr[i].amount);
+		printf("pubkey:\t%s\n", tx.out_arr[i].pubkey);                
+
+        }
+	printf("\n\tlocktime:\t%s\n",tx.locktime);
+
 	return 0;
 }
 
