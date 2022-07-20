a71fa0108f365a8ccb39d069ffb2ad33b635e6d8
riomonet <arizablozki@gmail.com>
2022-07-20 12:04:29 -0400

    implemented segwit parsing

--- parser.c
+++ parser.c
@@ -3,8 +3,6 @@
 #include <string.h>
 
 
-#define SEGWIT "0001"
-
 struct in {
 	
 	char			*txid;
@@ -17,22 +15,27 @@ struct out {
 	
 	char			*amount;
 	char			*pubkey;
+};
 
+struct segwit {
+
+	char			*segwit_data;
+	
 };
 
 struct transaction {
 	
 	char			version[9];
-	char			seg_mkrflag[5];
+	int			segwit;		/* boolean */
 	char			inputs[3];
 	struct in		*in_arr;
 	char			outputs[3];
 	struct out		*out_arr;
+	char			witness_cnt[3];
+	struct segwit		*witness;
 	char			locktime[9];
 };
 
-
-void sub_str(char *, char *, int, int);
 
 int main(int argc, char *argv[])
 {
@@ -55,16 +58,27 @@ int main(int argc, char *argv[])
 	value if necessary.
 	*/
 
-
 	for (i = 0; i < 8; ++i) {
 		tx.version[i] = *ptr++;
 	}
 	tx.version[i] = '\0';
 
-	for (i = 0; i < 2; ++i) {
+	
+
+        if (strncmp(ptr, "0001", 4) == 0) {
+		ptr += 4;
+		tx.segwit = 1;
+	} else {
+		tx.segwit = 0;
+	}
+
+	
+        for (i = 0; i < 2; ++i) {
 		tx.inputs[i] = *ptr++;
 	}
 	tx.inputs[i] = '\0';
+
+
 	
 	char *tmp = malloc(3 * sizeof(char));
 	int  script_len;
@@ -127,14 +141,41 @@ int main(int argc, char *argv[])
                 
 	}
 
+			
+	int wit_cnt = 0;
+
+        if (tx.segwit) {
+
+		for (i = 0; i < 2; ++i) {
+			tx.witness_cnt[i] = *ptr++;
+		}
+		tx.witness_cnt[2] = '\0';
+
+		wit_cnt = strtoul(tx.witness_cnt, &p, 16);
+		tx.witness = malloc(wit_cnt * sizeof(struct segwit ));
+		
+		for (i = 0; i < wit_cnt; ++i) {
+			for (j = 0; j < 2; ++j) {
+				tmp[j] = *ptr++;
+			}
+                        tmp[2] = '\0';
+
+			script_len = strtoul(tmp, &p, 16);
+			tx.witness[i].segwit_data = malloc((2 * script_len + 1));
+
+			for (j = 0; j < script_len * 2; ++j) 
+				tx.witness[i].segwit_data[j] = *ptr++;
+		}
+	}
+	
 	for (i = 0; i < 8; ++i) {
 		tx.locktime[i] = *ptr++;
 	}
 	tx.locktime[i] = '\0';
         
+	printf("\n********************************************************************************\n");
 
-
-        printf("\n\tverison:\t%s\n\n\tinput_cnt:\t%s",tx.version,tx.inputs);
+        printf("\n\tverison:\t%s\n\n\t\tinput_cnt:\t%s",tx.version,tx.inputs);
 
         for (i = 0; i < inp_cnt ; ++i) {
 		printf("\n__________________________________\n");
@@ -146,17 +187,29 @@ int main(int argc, char *argv[])
 		printf("seq:\t%s\n", tx.in_arr[i].sequence);
         }
 
-        printf("\n\toutput_cnt:\t%s",tx.outputs);
+        printf("\n\t\toutput_cnt:\t%s",tx.outputs);
 
         for (i = 0; i < output_cnt ; ++i) {
 		printf("\n__________________________________\n");
 		printf("%d\n",i + 1);
                 printf("amount:\t%s\n", tx.out_arr[i].amount);
-		printf("pubkey:\t%s\n", tx.out_arr[i].pubkey);                
+		printf("pubkey:\t%s\n", tx.out_arr[i].pubkey);
 
         }
-	printf("\n\tlocktime:\t%s\n",tx.locktime);
 
+	if (tx.segwit) {
+
+		printf("\n\t\twitness_cnt:\t%s",tx.witness_cnt);
+
+                for (i = 0; i < wit_cnt; ++i) {
+			printf("\n__________________________________\n");
+			printf("%d\n",i + 1);
+			printf("witness:\t%s\n", tx.witness[i].segwit_data);
+		}
+	}
+	
+	printf("\n\tlocktime:\t%s\n\n",tx.locktime);
+	printf("\n********************************************************************************\n");
 	return 0;
 }
 
